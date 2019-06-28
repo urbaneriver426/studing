589b0b86de7347efa25d1055088d856479cb87dd
urbaneriver426 <45755209+urbaneriver426@users.noreply.github.com>
2019-06-28 23:22:47 +0300

    Create DynArray.py

---
+++ DynArray.py
@@ -0,0 +1,60 @@
+import ctypes
+
+class DynArray:
+	
+	def __init__(self):
+		self.count = 0
+		self.capacity = 16
+		self.array = self.make_array(self.capacity)
+
+	def __len__(self):
+		return self.count
+
+	def make_array(self, new_capacity):
+		return (new_capacity * ctypes.py_object)()
+
+	def __getitem__(self,i):
+		if i < 0 or i >= self.count:
+			raise IndexError('Index is out of bounds')
+		return self.array[i]
+
+	def resize(self, new_capacity):
+		new_array = self.make_array(new_capacity)
+		for i in range(self.count):
+			new_array[i] = self.array[i]
+		self.array = new_array
+		self.capacity = new_capacity
+
+	def append(self, itm):
+		if self.count == self.capacity:
+			self.resize(2*self.capacity)
+		self.array[self.count] = itm
+		self.count += 1
+
+	def insert(self, i, itm):
+		if i < 0 or i > self.count or i > self.capacity:
+			raise IndexError('Index is out of bounds')
+		for j in range (i, self.count+1):
+			if j == i:
+				x = self.array[j]
+				self.array[j] = itm
+			elif j < self.count:
+				self.array[j], x = x, self.array[j]
+			else:
+				self.append(x)
+
+	def delete(self, i):
+		if i < 0 or i > self.count or i > self.capacity:
+			raise IndexError('Index is out of bounds')
+		for j in range(self.count-1, i-1, -1):
+			if j == self.count-1:
+				prev = self.array[j]
+			elif j != i:
+				self.array[j], prev = prev, self.array[j]
+			else:
+				self.array[j] = prev
+				self.count -= 1
+		new_capacity = self.capacity		
+		if self.capacity > 16 and self.count < self.capacity / 2:
+			new_capacity = int(self.capacity//1.5)
+		self.resize(new_capacity)
